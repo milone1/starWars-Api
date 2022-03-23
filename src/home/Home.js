818a2f76f8a09f4724b3e1da37c2ba8b8aed2e69
milone1 <erickmilan1002@gmail.com>
2022-03-23 16:03:28 -0500

    ultima-fase

--- src/home/Home.js
+++ src/home/Home.js
@@ -4,21 +4,42 @@ import { getDataFromStarWars } from "../peticion/Api";
 
 const Home = () => {
 
-    const[starWar,setStarWars] = useState([]);
-    const fetchStarWarList = async() => {
+    const[starWars,setStarWar] = useState([]);
+
+    const fetchStarWarsList = async() => {
+        
         const listStarWars = await getDataFromStarWars();
-        console.log(listStarWars)
-    }
+        
+        console.log(listStarWars);
+
+        setStarWar(listStarWars);
+    };
+
     //* esta funcion se ejecuta cuando la pagina esta cargando.
     useEffect(() => {
-        fetchStarWarList();
+        fetchStarWarsList();
     }, []);
 
     return (
         <Container>
-            <h1>STAR WARS API :V</h1>
-            
-
+            <Grid container spacing={3}>
+                {starWars.map( starWar =>(
+                    <Grid item md={4} lg={4} sm={12} xs={12}>
+                        <h1>{starWar.name}</h1>
+                        <Card sx={{ width: 300}}>
+                            <CardMedia component='img' image={`${starWar.image}`} />
+                            <CardContent>
+                                <h4>NOMBRE:</h4>
+                                <h6>{starWar.name}</h6>
+                                <h4>TALLA:</h4>
+                                <h6>{starWar.height}</h6>
+                                <h4>ID:</h4>
+                                <h6>{starWar.id}</h6>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))} 
+            </Grid>
         </Container>
     );
 }
